846e3edcb54cdbb016a0e31762886263e9e2d410
Vincent_Passanisi <vince@santafeimporters.com>
2023-03-26 16:26:57 -0700

    Updating html, js, and css files

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,113 @@
 var sidebarOpen = false;
-var didebar = document.getElementById("sidebar");
+var sidebar = document.getElementById("sidebar");
 
 function openSidebar() {
-    if(sidebarOpen) {
-        sidebar 
+    if(!sidebarOpen) {
+        sidebar.classList.add("sidebar-responsive");
+        sidebarOpen = true; 
     }
 }
+
+function closeSidebar () {
+    if(sidebarOpen) {
+        sidebar.classList.remove("sidebar-responsive");
+        sidebarOpen = false;
+    }
+}
+
+// --------------------- CHARTS ---------------------
+
+// BAR CHART
+
+var barChartOptions = {
+    series: [{
+    data: [3171, 3164]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350,
+    toolbar: {
+      show: false
+    },
+  },
+  colors: [
+    "#246dec",
+    "#cc3c43",
+    "#367952",
+    "#f5b74f",
+    "#4f35a1"
+  ],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      horizontal: false,
+      columnWidth: '40%',
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  legend: {
+    show: false
+  },
+  xaxis: {
+    categories: ['Real', 'Fake'],
+  },
+  yaxis: {
+    title: {
+      text: "Count"
+    }
+  }
+};
+
+  var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+  barChart.render();
+
+  var areaChartOptions = {
+    series: [{
+    name: 'Titles',
+    data: [7, 12, 5, 11, 15, 9, 8]
+  }, {
+    name: 'Articles',
+    data: [1100, 950, 1360, 2100, 4200, 850, 1250]
+  }],
+    chart: {
+    height: 350,
+    type: 'area',
+    toolbar: {
+    },
+  },
+  colors: ["#4f35a1", "#246dec"],
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  
+  labels: ['1', '2','3','4','5','6','7'],
+  markers: {
+    size: 0
+  },
+  yaxis: [
+    {
+      title: {
+        text: 'Titles',
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: 'Articles',
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  }
+};
+
+  var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+  areaChart.render();
